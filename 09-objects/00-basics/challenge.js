0d38557e0308c85332813e201a13e43df39885d2
svalaer <sullivanvalaer@gmail.com>
2015-01-18 15:58:22 -0800

    09-objects wrote functions that would make the mocha tests pass

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,30 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function (title, duration, students){
+    var course = {
+        title: title,
+        duration: duration,
+        students: students
+    };
+    return course;
+};
 
-module.exports.addProperty = undefined;
+module.exports.addProperty = function (object,newProp,newValue){
+    if (!object[newProp]) {
+        object[newProp] = newValue;
+    }
+    return object;
+}
 
-module.exports.formLetter = undefined;
+module.exports.formLetter = function (letter){
+    var messageStr = "Hello" + " " + letter.recipient + ",\n\n" + letter.msg + "\n\n\Sincerely,\n" + letter.sender;
+    return messageStr;
+};
 
-module.exports.canIGet = undefined;
+module.exports.canIGet = function(item, money) {
+    var products = {
+        'MacBook Air': 999,
+        'MacBook Pro': 1299,
+        'Mac Pro': 2499,
+        'Apple Sticker': 1
+    };
+    return products[item] <= money;
+};
